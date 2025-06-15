de953ca5bacda7ba477b670e8dfb3fabe7c4ea69
wertas441 <wertas.borisov@gmail.com>
2025-06-15 23:10:54 +0300

    develop-frontend: some little work on list until diplom

--- frontend/src/app/shop/catalog/keys/KeysCatalog.tsx
+++ frontend/src/app/shop/catalog/keys/KeysCatalog.tsx
@@ -23,14 +23,25 @@ export default function KeysCatalog({keysArray}: KeysCatalogProps) {
         <>
             <div className="flex container mx-auto items-center my-10">
 
-                <div className="">
+                <div className="text-white">
                     фильтры
                 </div>
 
                 <div className="">
                     {keysArray.map((game) => (
-                        <div key={game.id} className={`flex items-center ${styles.mainCard}`}>
-                          <Image src = {game.picture} width = {220} height = {220} alt = {game.name} />
+                        <div key={game.id} className={`flex items-center py-4 px-6 rounded-lg ${styles.mainCard}`}>
+                          <Image className={`rounded-md`} src = {game.picture} width = {230} height = {230} alt = {game.name} />
+                            <div className="flex items-center space-x-45 pl-6">
+                                <div className="">
+                                    <h1 className={`text-white`}>{game.name}</h1>
+                                    <p className={`text-white`}>Дата выхода: {game.releaseData}</p>
+                                </div>
+
+                                <div className="">
+                                    <h1 className={`text-white font-bold text-3xl`}>{game.price} р</h1>
+                                    <button className={`text-black font-semibold p-2 mt-5 rounded-md ${styles.addButton}`}>Добавить в корзину</button>
+                                </div>
+                            </div>
                         </div>
                     ))}
                 </div>
