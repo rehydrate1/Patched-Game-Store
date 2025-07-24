bdc8f0d3fcde82ab070350a032c90748deb910a2
wertas441 <wertas44187@gmail.com>
2025-07-24 15:26:45 +0300

    before changes to dash

--- frontend/src/app/profile/Profile.tsx
+++ frontend/src/app/profile/Profile.tsx
@@ -1,23 +1,30 @@
 "use client";
 
 import { useState } from 'react';
-import { UserCircleIcon, LockClosedIcon, ShoppingBagIcon, ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
+import {
+    UserCircleIcon,
+    EnvelopeIcon,
+    LockClosedIcon,
+    ShoppingBagIcon,
+    ArrowLeftOnRectangleIcon
+} from '@heroicons/react/24/outline';
 import AccountProfile from "@/components/Profile/AccountProfile";
 import ChangePassword from '@/components/Profile/ChangePassword';
 import PurchaseHistory from '@/components/Profile/PurchaseHistory';
 import ChangeEmail from '@/components/Profile/ChangeEmail';
-import {HeartIcon} from "@heroicons/react/16/solid";
 
-export default function ProfileClient() {
+export default function ProfileClient({menuItems}) {
+
     const [activeTab, setActiveTab] = useState('profile');
 
-    const menuItems = [
-        { id: 'profile', label: 'Профиль', icon: UserCircleIcon },
-        { id: 'history', label: 'История покупок', icon: ShoppingBagIcon },
-        { id: 'email', label: 'Сменить почту', icon: LockClosedIcon },
-        { id: 'password', label: 'Сменить пароль', icon: LockClosedIcon },
+    const icons = {
+        UserCircleIcon: UserCircleIcon,
+        EnvelopeIcon: EnvelopeIcon,
+        LockClosedIcon: LockClosedIcon,
+        ShoppingBagIcon: ShoppingBagIcon,
+        ArrowLeftOnRectangleIcon: ArrowLeftOnRectangleIcon,
+    }
 
-    ];
 
     const renderContent = () => {
         switch (activeTab) {
@@ -36,28 +43,32 @@ export default function ProfileClient() {
 
     return (
         <div className="text-white min-h-screen w-full p-4 mt-6 sm:p-6 lg:p-8">
-            <div className="w-full max-w-screen-xl mx-auto flex flex-col md:flex-row gap-8">
+            <div className="w-full container mx-auto flex flex-col md:flex-row gap-8">
 
                 {/* === БОКОВОЕ МЕНЮ (SIDEBAR) === */}
                 <aside className="w-full md:w-1/4 lg:w-1/5">
                     <nav className="space-y-2 bg-[#212227] p-4 rounded-xl border border-white/10">
-                        {menuItems.map((item) => (
-                            <button
-                                key={item.id}
-                                onClick={() => setActiveTab(item.id)}
-                                className={`w-full flex items-center gap-3 py-3 px-4 rounded-lg transition-colors text-left ${
-                                    activeTab === item.id
-                                        ? 'bg-[#00FE92] text-black font-bold'
-                                        : 'text-gray-300 hover:bg-gray-700/50'
-                                }`}
-                            >
-                                <item.icon className="h-6 w-6" />
-                                <span className="text-md">{item.label}</span>
-                            </button>
-                        ))}
+                        {menuItems.map((item) => {
+
+                            const IconComponent = icons[item.icon]
+                            return (
+                                <button
+                                    key={item.id}
+                                    onClick={() => setActiveTab(item.id)}
+                                    className={`w-full flex items-center gap-3 py-3 cursor-pointer px-4 rounded-lg transition-colors text-left ${
+                                        activeTab === item.id
+                                            ? 'bg-[#00FE92] text-black font-bold'
+                                            : 'text-gray-300 hover:bg-gray-700/50'
+                                    }`}
+                                >
+                                    <IconComponent className="h-6 w-6"/>
+                                    <span className="text-md">{item.label}</span>
+                                </button>
+                            )
+                        })}
                         <div className="border-t border-white/10 my-2"></div>
                         <button
-                            className="w-full flex items-center gap-3 py-3 px-4 rounded-lg transition-colors text-left text-gray-300 hover:bg-red-800/50"
+                            className="w-full flex items-center cursor-pointer gap-3 py-3 px-4 rounded-lg transition-colors text-left text-gray-300 hover:bg-red-800/50"
                         >
                             <ArrowLeftOnRectangleIcon className="h-6 w-6" />
                             <span className="text-md">Выйти</span>
--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -8,10 +8,22 @@ export const metadata = {
 
 export default function ProfilePage() {
 
+    interface MenuItems{
+        id: string;
+        label: string;
+        icon: 'UserCircleIcon' | 'ShoppingBagIcon' | 'EnvelopeIcon' | 'LockClosedIcon' ;
+    }
+
+    const menuItems: MenuItems [] = [
+        { id: 'profile', label: 'Профиль', icon: 'UserCircleIcon' },
+        { id: 'history', label: 'История покупок', icon: 'ShoppingBagIcon' },
+        { id: 'email', label: 'Сменить почту', icon: 'EnvelopeIcon' },
+        { id: 'password', label: 'Сменить пароль', icon: 'LockClosedIcon' },
+    ];
 
     return (
         <>
-            <Profile />
+            <Profile menuItems = {menuItems}  />
         </>
     )
 }
